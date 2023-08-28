7f06e177904d1120d1b8686b9f6a648667e19adb
江鹏利 <845415120@qq.com>
2023-08-28 18:03:51 +0800

    首页-04

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <van-tabs shrink sticky v-model:active="active">
+      <van-tab title="关注" name="like"
+        ><knowledge-list type="like" />
+      </van-tab>
+      <van-tab title="推荐" name="recommend"
+        ><knowledge-list type="recommend"
+      /></van-tab>
+      <van-tab title="减脂" name="fatReduction"
+        ><knowledge-list type="fatReduction"
+      /></van-tab>
+      <van-tab title="饮食" name="food"><knowledge-list type="food" /></van-tab>
+    </van-tabs>
+  </div>
 </template>
 
-<script setup  >
+<script setup lang="ts">
+import KnowledgeList from "./components/KnowledgeList.vue";
+import { ref } from "vue";
+import type { KnowledgeType } from "@/types/consult";
 
+const active = ref<KnowledgeType>("recommend");
 </script>
 
-<style  lang="scss" scoped  >
-
-</style>
+<style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001, // 更换成你想要的端口号
+  },
   plugins: [
     vue(),
     Components({
